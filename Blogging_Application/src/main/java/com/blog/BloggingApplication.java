3a9d0efd99c79edd07112f4b8b8fefe3a8422200
anushka-u <anushkau032@gmail.com>
2024-05-13 21:49:27 +0530

    Blogging Application

--- Blogging_Application/src/main/java/com/blog/BloggingApplication.java
+++ Blogging_Application/src/main/java/com/blog/BloggingApplication.java
@@ -2,6 +2,8 @@ package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.*;
 
 @SpringBootApplication
 public class BloggingApplication {
@@ -9,6 +11,10 @@ public class BloggingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BloggingApplication.class, args);
 	}
-
+	
+	@Bean
+	public ModelMapper createModelMapper() {
+		return new ModelMapper();
+	}
 }
 
