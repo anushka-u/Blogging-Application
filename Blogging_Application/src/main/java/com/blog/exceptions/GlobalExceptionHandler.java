ecc585692c93690ea7db0656a3e1d52e2b230f30
anushka-u <anushkau032@gmail.com>
2024-05-13 10:40:09 +0530

    Global Exception Handler

---
+++ Blogging_Application/src/main/java/com/blog/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.blog.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.blog.payloads.ApiResponse;
+
+
+// This below annotation will represent our class as exception handler class, it will scan all of our controllers
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
+		String message = ex.getMessage();
+		ApiResponse apiResponse = new ApiResponse(message,false);
+		return new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.NOT_FOUND);
+	}
+
+}
